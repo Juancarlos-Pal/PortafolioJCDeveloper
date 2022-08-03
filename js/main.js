e242b5452ad773c3d2c20c41892a0647de884e36
Juancarlos-Pal <39867148+Juancarlos-Pal@users.noreply.github.com>
2022-08-03 14:23:51 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+(function() {
+    "use strict";
+  
+    /**
+     * Hero type effect
+     */
+     var typed = new Typed(".typed", {
+        strings: ["Ingeniero de Software", "Desarrollador front end"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true,
+      });
+
+    
+  
+  })()
