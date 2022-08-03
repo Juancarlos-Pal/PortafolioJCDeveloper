9c02baed0b2f7a7ab5dfb0f50047dae0f0705af4
Juancarlos-Pal <39867148+Juancarlos-Pal@users.noreply.github.com>
2022-08-03 14:54:35 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
      * Hero type effect
      */
      var typed = new Typed(".typed", {
-        strings: ["Ingeniero de Software", "Desarrollador front end"],
+        strings: ["Ingeniero de Software", "Desarrollador Web"],
         typeSpeed: 50,
         backSpeed: 50,
         loop: true,
